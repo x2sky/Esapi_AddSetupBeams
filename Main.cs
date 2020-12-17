dbed283162abeb93167149f7468e6943816d4c49
Becket Hui <x2sky@hotmail.com>
2020-12-17 11:04:12 -0700

    Add project files.

---
+++ Main.cs
@@ -0,0 +1,133 @@
+//////////////////////////////////////////////////////////////////////
+///This program adds 3 kV setup beams and 1 CBCT setup beam to the current plan
+///
+///--version 0.0
+///Becket Hui 2020/12
+//////////////////////////////////////////////////////////////////////
+using System;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using VMS.TPS.Common.Model.API;
+using VMS.TPS.Common.Model.Types;
+using addSetupBeams;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+
+// TODO: Replace the following version attributes by creating AssemblyInfo.cs. You can do this in the properties of the Visual Studio project.
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyInformationalVersion("1.0")]
+
+// TODO: Uncomment the following line if the script requires write access.
+[assembly: ESAPIScript(IsWriteable = true)]
+
+namespace VMS.TPS
+{
+    public class Script
+    {
+        public Script()
+        {
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public void Execute(ScriptContext context)
+        {
+            // First check version of the ESAPI dll
+            String esapiVer = FileVersionInfo.GetVersionInfo(Assembly.GetAssembly(typeof(ExternalPlanSetup)).Location).FileVersion;
+            Match mEsapiVer = Regex.Match(esapiVer, @"^(\d+).");
+            if (mEsapiVer.Success)
+            {
+                int esapiVer0 = Int32.Parse(mEsapiVer.Groups[1].Value);
+                if (esapiVer0 < 16)
+                    throw new ApplicationException("ESAPI ver." + esapiVer + ", script cannot run on version below 16.");
+            }
+
+            // Open current patient
+            Patient currPt = context.Patient;
+            // If there's no selected patient, throw an exception
+            if (currPt == null)
+                throw new ApplicationException("Please open a patient before using this script.");
+
+            // Open current plan
+            ExternalPlanSetup currPln = context.ExternalPlanSetup;
+            // If there's no selected plan, throw an exception
+            if (currPln == null)
+                throw new ApplicationException("Please select a plan before using this script.");
+
+            // Get first beam
+            Beam currBm = currPln.Beams.FirstOrDefault();
+            // If there's no beam in plan, throw an exception
+            if (currBm == null)
+                throw new ApplicationException("Please add a beam with valid isocenter in current plan before using this script.");
+
+            // Get patient orientation
+            String ptOrient = "";
+            switch (currPln.TreatmentOrientation)
+            {
+                case PatientOrientation.HeadFirstSupine:
+                    ptOrient = "HFS";
+                    break;
+                case PatientOrientation.HeadFirstProne:
+                    ptOrient = "HFP";
+                    break;
+                case PatientOrientation.FeetFirstSupine:
+                    ptOrient = "FFS";
+                    break;
+                case PatientOrientation.FeetFirstProne:
+                    ptOrient = "FFP";
+                    break;
+                default:
+                    ptOrient = "UK";
+                    break;
+            }
+            if (ptOrient == "UK")
+                throw new ApplicationException("Cannot determine beam orientation relative to patient orientation, no setup beam created.");
+
+            // Create DRR parameters
+            DRRCalculationParameters drrParam = new DRRCalculationParameters(500);  // 50 cm DRR size
+            if (currPln.Id.ToUpper().Contains("BREAST"))  // breast plan uses chest DRR setting
+            {
+                drrParam.SetLayerParameters(0, 0.6, -990.0, 0.0, 1.0, 5.0);
+                drrParam.SetLayerParameters(1, 0.1, -450.0, 0.0, -4.0, 8.0);
+                drrParam.SetLayerParameters(2, 1.0, 100.0, 1000.0);
+            }
+            else  // all other plan uses the default DRR setting
+            {
+                drrParam.SetLayerParameters(0, 2.0, 0.0, 130.0, -100.0, 100.0);
+                drrParam.SetLayerParameters(1, 10.0, 100.0, 1000.0, -100.0, 100.0);
+            }
+
+            // Determine beam number based on the largest number currently in the setup beam ID under the current patient
+            int bmIdNo = 0;
+            foreach (Course crs in currPt.Courses)
+            {
+                foreach (ExternalPlanSetup pln in crs.ExternalPlanSetups)
+                {
+                    foreach (Beam bm in pln.Beams)
+                    {
+                        Match bmId = Regex.Match(bm.Id, @"^[A-Z](\d+)$");
+                        if (bmId.Success)
+                        {
+                            bmIdNo = Math.Max(bmIdNo, Int32.Parse(bmId.Groups[1].Value));
+                        }
+                    }
+                }
+            }
+            bmIdNo = bmIdNo + 1;
+
+            // Create setup beam
+            currPt.BeginModifications();
+            SetupBeam setupBm = new SetupBeam(currBm);
+            setupBm.AddBeam(currPln, Double.NaN, ptOrient, drrParam, bmIdNo);  // CBCT
+            setupBm.AddBeam(currPln, 0.0, ptOrient, drrParam, bmIdNo);
+            setupBm.AddBeam(currPln, 270.0, ptOrient, drrParam, bmIdNo);
+            setupBm.AddBeam(currPln, 90.0, ptOrient, drrParam, bmIdNo);
+
+            MessageBox.Show("Set of setup beam has been created in plan " + currPln.Id + ".");
+        }
+    }
+}
---
+++ SetupBeam.cs
@@ -0,0 +1,77 @@
+﻿//////////////////////////////////////////////////////////////////////
+///Class and functions to add setup beam
+/// Include functions:
+///     SetupBeam(Beam) - initialze function, creates BeamMachineParameters
+///     AddBeam(PlanSetup, GantryAngle, PtOrientation, DRRParameters, BmNum) - add a setup beam to the plan based on gantry angle
+///     CheckBeamDir(GantryAngle, PtOrientation) - Find beam orietation based on beam angle and patient orientation
+///     
+///--version 0.0
+///Becket Hui 2020/12
+//////////////////////////////////////////////////////////////////////
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using VMS.TPS.Common.Model.API;
+using VMS.TPS.Common.Model.Types;
+
+namespace addSetupBeams
+{
+    class SetupBeam
+    {
+        private ExternalBeamMachineParameters bmMachParam;
+        private VVector plnIso;
+        public SetupBeam(Beam currBm)
+        {
+            // Create machine parameters
+            String energy = currBm.EnergyModeDisplayName;
+            String fluence = null;
+            Match EMode = Regex.Match(currBm.EnergyModeDisplayName, @"^([0-9]+[A-Z]+)-?([A-Z]+)?", RegexOptions.IgnoreCase);  //format is... e.g. 6X(-FFF)
+            if (EMode.Success)
+            {
+                if (EMode.Groups[2].Length > 0)  // fluence mode
+                {
+                    energy = EMode.Groups[1].Value;
+                    fluence = EMode.Groups[2].Value;
+                } // else normal modes uses default in decleration
+            }
+            bmMachParam = new ExternalBeamMachineParameters(currBm.TreatmentUnit.Id.ToString(), energy, currBm.DoseRate, "STATIC", fluence);
+            plnIso = currBm.IsocenterPosition;  // copy isocenter location
+        }
+        public void AddBeam(ExternalPlanSetup currPln, Double gantryAng, String ptOrient, DRRCalculationParameters drrParam, int bmIdNo)
+        {
+            if (Double.IsNaN(gantryAng))  // Gantry angle = Nan for CBCT
+            {
+                Beam bm = currPln.AddSetupBeam(bmMachParam, new VRect<double>(-50.0, -50.0, 50.0, 50.0), 0.0, 0.0, 0.0, plnIso);
+                bm.Id = "C" + bmIdNo.ToString();
+                bm.Name = "CBCT setup";
+            }
+            else  // kV beam
+            {
+                // adding 10 x 10 field, 0 coll rotation and 0 couch angle
+                Beam bm = currPln.AddSetupBeam(bmMachParam, new VRect<double>(-50.0, -50.0, 50.0, 50.0), 0.0, gantryAng, 0.0, plnIso);
+                String bmDir = CheckBeamDir(gantryAng, ptOrient);
+                bm.Id = bmDir[0].ToString().ToUpper() + bmIdNo.ToString();
+                bm.Name = bmDir + " kV setup";
+                // create DRR
+                bm.CreateOrReplaceDRR(drrParam);
+            }
+        }
+        private String CheckBeamDir(Double gantryAng, String ptOrient)
+        {
+            switch (gantryAng)
+            {
+                case 0.0:
+                    if (ptOrient == "HFS" || ptOrient == "FFS") return "anterior"; else return "posterior";
+                case 270.0:
+                    if (ptOrient == "HFS" || ptOrient == "FFP") return "right"; else return "left";
+                case 90.0:
+                    if (ptOrient == "HFS" || ptOrient == "FFP") return "left"; else return "right";
+                default:
+                    return "unknown";
+            }
+        }
+    }
+}
